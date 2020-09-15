cdfc6f3360a3984109fc7af62963e0f0ca2ac477
losfair <zhy20000919@hotmail.com>
2020-09-15 20:02:53 +0800

    Memory encoding script.

---
+++ Scripts/memory_encode.py
@@ -0,0 +1,23 @@
+import sys
+import binascii
+
+def mkBinStr(x):
+    s = ""
+    for j in range(0, 64):
+        i = 64 - j - 1
+        if (x >> i) & 1 == 1:
+            s += "1"
+        else:
+            s += "0"
+    return s
+
+data = sys.stdin.buffer.read()
+while len(data) % 8 != 0:
+    data += b"\x00"
+
+for i in range(0, 65536, 8):
+    if i < len(data):
+        x = int.from_bytes(data[i:i+8], "little")
+    else:
+        x = 0
+    print(mkBinStr(x))
