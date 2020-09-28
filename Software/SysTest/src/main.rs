909c6879013b04b0416930efaa7afa1ee2b3c66c
losfair <zhy20000919@hotmail.com>
2020-09-28 17:26:49 +0800

    M support.

--- Software/SysTest/src/main.rs
+++ Software/SysTest/src/main.rs
@@ -18,6 +18,18 @@ pub static mut SOME_M: u32 = 200;
 #[no_mangle]
 pub static mut SOME_BITS: &'static [u8] = &[0x12, 0x34, 0x56, 0x78];
 
+#[no_mangle]
+pub static mut DIV_A: u32 = 231;
+
+#[no_mangle]
+pub static mut DIV_A_S: i32 = 231;
+
+#[no_mangle]
+pub static mut DIV_B: u32 = 40;
+
+#[no_mangle]
+pub static mut DIV_B_S: i32 = 40;
+
 unsafe fn putchar(c: u8) {
     core::ptr::write_volatile(0xfe000000 as *mut u32, c as u32);
 }
@@ -30,13 +42,34 @@ unsafe fn cycle() -> u64 {
 
 #[no_mangle]
 pub unsafe extern "C" fn rust_main() -> ! {
+    // Comment cycle() out for co-simulation
+    /*
     let start = cycle();
-    println!("Hello world from Rust on Violet!");
+    println!("Hello world from Rust on Violet.");
     let end = cycle();
     println!("Previous println took {} cycles.", end - start);
+    */
 
-    println!("Some const 1: {}", SOME_N); // TODO: Incorrect number here
+    println!("Some const 1: {}", SOME_N);
     println!("Some const 2: {}", SOME_M);
+
+    println!("Mul A*B: {}", DIV_A * DIV_B);
+
+    println!("UDiv A/B: {}", DIV_A / DIV_B);
+    println!("URem A/B: {}", DIV_A % DIV_B);
+
+    println!("SDiv A/B: {}", DIV_A_S / DIV_B_S);
+    println!("SRem A/B: {}", DIV_A_S % DIV_B_S);
+
+    println!("SDiv -A/B: {}", -DIV_A_S / DIV_B_S);
+    println!("SRem -A/B: {}", -DIV_A_S % DIV_B_S);
+
+    println!("SDiv A/-B: {}", DIV_A_S / -DIV_B_S);
+    println!("SRem A/-B: {}", DIV_A_S % -DIV_B_S);
+
+    println!("SDiv -A/-B: {}", -DIV_A_S / -DIV_B_S);
+    println!("SRem -A/-B: {}", -DIV_A_S % -DIV_B_S);
+
     loop {
         llvm_asm!("" :::: "volatile");
     }
