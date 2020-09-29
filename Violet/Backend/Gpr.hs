2589dc26f306149b1a88d344fae5d6b47142daa3
losfair <zhy20000919@hotmail.com>
2020-09-29 16:08:06 +0800

    Use async ram for register file.

--- Violet/Backend/Gpr.hs
+++ Violet/Backend/Gpr.hs
@@ -4,37 +4,64 @@ import Clash.Prelude
 import Violet.Types.Gpr
 import qualified Violet.Types.Fifo as FifoT
 
-type RegFile = Vec 32 RegValue
-
-gpr' :: (RegFile, ((RegValue, RegValue), (RegValue, RegValue)))
-     -> ((FifoT.FifoItem, FifoT.FifoItem), WritePort, WritePort)
-     -> ((RegFile, ((RegValue, RegValue), (RegValue, RegValue))), ((RegValue, RegValue), (RegValue, RegValue)))
-gpr' (rf, results) ((item1, item2), wp1, wp2) = ((rf', results'), results)
-    where
-        rf_ = case wp1 of
-            Just (i, v) -> replace i v rf
-            Nothing -> rf
-        rf' = case wp2 of
-            Just (i, v) -> replace i v rf_
-            Nothing -> rf_
-        (r1, r2) = fifoItemToRegIndices item1
-        (r3, r4) = fifoItemToRegIndices item2
-        results' = ((regFetch rf' r1, regFetch rf' r2), (regFetch rf' r3, regFetch rf' r4))
-
 gpr :: HiddenClockResetEnable dom
     => Signal dom ((FifoT.FifoItem, FifoT.FifoItem), WritePort, WritePort)
     -> Signal dom ((RegValue, RegValue), (RegValue, RegValue))
-gpr = mealy gpr' (repeat 0, ((0, 0), (0, 0)))
+gpr inputs = bundle (bundle (read1, read2), bundle (read3, read4))
+    where
+        (inPorts, wp1, wp2) = unbundle inputs
+        (port1, port2) = unbundle inPorts
+        (i1, i2) = unbundle $ fmap fifoItemToRegIndices port1
+        (i3, i4) = unbundle $ fmap fifoItemToRegIndices port2
 
-fifoItemToRegIndices :: FifoT.FifoItem -> (RegIndex, RegIndex)
+        readA1 = asyncRamPow2 i1 (fmap transformWp wp1)
+        readA2 = asyncRamPow2 i2 (fmap transformWp wp1)
+        readA3 = asyncRamPow2 i3 (fmap transformWp wp1)
+        readA4 = asyncRamPow2 i4 (fmap transformWp wp1)
+        readB1 = asyncRamPow2 i1 (fmap transformWp wp2)
+        readB2 = asyncRamPow2 i2 (fmap transformWp wp2)
+        readB3 = asyncRamPow2 i3 (fmap transformWp wp2)
+        readB4 = asyncRamPow2 i4 (fmap transformWp wp2)
+
+        (a1, a2, a3, a4) = unbundle $ (mealy mkAllocation 0) (bundle (wp1, wp2, i1, i2, i3, i4))
+
+        read1 = register undefined $ fmap selectReadRes $ bundle (a1, i1, readA1, readB1, wp1, wp2)
+        read2 = register undefined $ fmap selectReadRes $ bundle (a2, i2, readA2, readB2, wp1, wp2)
+        read3 = register undefined $ fmap selectReadRes $ bundle (a3, i3, readA3, readB3, wp1, wp2)
+        read4 = register undefined $ fmap selectReadRes $ bundle (a4, i4, readA4, readB4, wp1, wp2)
+
+fifoItemToRegIndices :: FifoT.FifoItem -> (Unsigned 5, Unsigned 5)
 fifoItemToRegIndices item = (rs1, rs2)
     where
         inst = case item of
             FifoT.Item (_, inst, _) -> inst
-            _ -> 0
-        rs1 = slice d19 d15 inst
-        rs2 = slice d24 d20 inst
+            _ -> undefined
+        rs1 = unpack $ slice d19 d15 inst
+        rs2 = unpack $ slice d24 d20 inst
 
-regFetch :: RegFile -> RegIndex -> RegValue
-regFetch rf 0 = 0
-regFetch rf i = rf !! i
+transformWp :: WritePort -> Maybe (Unsigned 5, RegValue)
+transformWp (Just (i, v)) = Just (unpack i, v)
+transformWp _ = Nothing
+
+mkAllocation :: BitVector 32
+             -> (WritePort, WritePort, Unsigned 5, Unsigned 5, Unsigned 5, Unsigned 5)
+             -> (BitVector 32, (Bool, Bool, Bool, Bool))
+mkAllocation s (wp1, wp2, i1, i2, i3, i4) = (s', (a1, a2, a3, a4))
+    where
+        s_ = case wp1 of
+            Just (i, _) -> clearBit s (fromIntegral i)
+            _ -> s
+        s' = case wp2 of
+            Just (i, _) -> setBit s_ (fromIntegral i)
+            _ -> s_
+        a1 = testBit s (fromIntegral i1)
+        a2 = testBit s (fromIntegral i2)
+        a3 = testBit s (fromIntegral i3)
+        a4 = testBit s (fromIntegral i4)
+
+selectReadRes :: (Bool, Unsigned 5, RegValue, RegValue, WritePort, WritePort) -> RegValue
+selectReadRes (_, 0, _, _, _, _) = 0
+selectReadRes (_, i, _, _, _, Just (i', v)) | i == unpack i' = v
+selectReadRes (_, i, _, _, Just (i', v), _) | i == unpack i' = v
+selectReadRes (False, _, a, _, _, _) = a
+selectReadRes (True, _, _, b, _, _) = b
