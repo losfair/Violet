d97f0bbdba9ea301bd665a468e7360a906830637
losfair <zhy20000919@hotmail.com>
2020-11-03 15:20:46 +0800

    Use builtin clash.

--- gen.sh
+++ gen.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-CLASH_DIR="/home/zhy/Projects/clash-compiler"
-PROJECT_DIR="$(pwd)"
-
-cd "$CLASH_DIR" || exit 1
-stack run --cwd "$PROJECT_DIR" -- clash --verilog Violet.Gen.CoreGen
+clash --verilog Violet.Gen.CoreGen
--- gen_exe.sh
+++ gen_exe.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-CLASH_DIR="/home/zhy/Projects/clash-compiler"
-PROJECT_DIR="$(pwd)"
-
-cd "$CLASH_DIR" || exit 1
-stack run --cwd "$PROJECT_DIR" -- clash VioletCore
+clash VioletCore
